ede57b222bb58dda47b03aecb0fe6bca31a1fc6e
LamichhaneBibek <lamichhane.kebib@gmail.com>
2025-01-10 13:09:19 +0545

    Add GetQuizById and UpdateQuiz methods to QuizService for quiz retrieval and modification

--- backend/internal/service/quiz_service.go
+++ backend/internal/service/quiz_service.go
@@ -1,8 +1,11 @@
 package service
 
 import (
+	"errors"
+
 	"github.com/LamichhaneBibek/quiz-webapp/internal/collection"
 	"github.com/LamichhaneBibek/quiz-webapp/internal/entity"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type QuizService struct {
@@ -17,4 +20,23 @@ func NewQuizService(quizCollection *collection.QuizCollection) *QuizService {
 
 func (qs *QuizService) GetAllQuizzes() ([]entity.Quiz, error) {
 	return qs.quizCollection.GetAllQuizzes()
+}
+
+func (qs QuizService) GetQuizById(id primitive.ObjectID) (*entity.Quiz, error) {
+	return qs.quizCollection.GetQuizByID(id)
+}
+
+func (qs QuizService) UpdateQuiz(id primitive.ObjectID, name string, questions []entity.QuizQuestion) error {
+	quiz, err := qs.quizCollection.GetQuizByID(id)
+	if err != nil {
+		return err
+	}
+
+	if quiz == nil {
+		return errors.New("quiz not found")
+	}
+
+	quiz.Name = name
+	quiz.Questions = questions
+	return qs.quizCollection.UpdateQuiz(*quiz)
 }
